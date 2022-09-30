c0b55592b872fd367600b4bf51c3ef741434362f
DominicSieli <dominicsieli@gmail.com>
2022-09-29 18:35:05 -0700

    Added quotes.js

---
+++ source/quotes.js
@@ -0,0 +1,14 @@
+const quotes = [
+	{
+		"text": "Never give up, never surrender.",
+		"author": "Tim Allan"
+	},
+	{
+		"text": "Do, or do not. There is no try.",
+		"author": "Yoda"
+	},
+	{
+		"text": "To infinity and beyond.",
+		"author": "Buzz Lightyear"
+	}
+]
--- source/script.js
+++ source/script.js
@@ -1,1 +1,19 @@
-console.log('testing');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const nextButton = document.getElementById('next-button');
+const quoteContainer = document.getElementById('quote-container');
+
+function loadQuote()
+{
+	const quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+	if(quote.text.length > 100) quoteText.classList.add('long-quote');
+	else quoteText.classList.remove('long-quote');
+
+	quoteText.textContent = `"${quote.text}"`;
+	authorText.textContent = `-${quote.author}`;
+}
+
+nextButton.addEventListener('click', loadQuote);
+
+loadQuote();
