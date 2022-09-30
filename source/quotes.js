b1a84970d967f2e655cbeab757f5495cf58dd4f6
DominicSieli <dominicsieli@gmail.com>
2022-09-30 10:12:52 -0700

    Added new quotes

--- source/quotes.js
+++ source/quotes.js
@@ -1,14 +1,122 @@
 const quotes = [
 	{
-		"text": "Never give up, never surrender.",
-		"author": "Tim Allan"
+		"text": "You have power over your mind – not outside events. Realize this, and you will find strength.",
+		"author": "Marcus Aurelius"
 	},
 	{
-		"text": "Do, or do not. There is no try.",
-		"author": "Yoda"
+		"text": "When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love.",
+		"author": "Marcus Aurelius"
 	},
 	{
-		"text": "To infinity and beyond.",
-		"author": "Buzz Lightyear"
+		"text": "The happiness of your life depends upon the quality of your thoughts.",
+		"author": "Marcus Aurelius"
+	},
+	{
+		"text": "If it is not right do not do it; if it is not true do not say it.",
+		"author": "Marcus Aurelius"
+	},
+	{
+		"text": "Whenever you are about to find fault with someone, ask yourself the following question: What fault of mine most nearly resembles the one I am about to criticize?",
+		"author": "Marcus Aurelius"
+	},
+	{
+		"text": "People are not disturbed by things, but by the views they take of them.",
+		"author": "Marcus Aurelius"
+	},
+	{
+		"text": "Do not indulge in dreams of having what you have not, but reckon up the chief of the blessings you do possess, and then thankfully remember how you would crave for them if they were not yours.",
+		"author": "Marcus Aurelius"
+	},
+	{
+		"text": "The soul becomes dyed with the color of its thoughts.",
+		"author": "Marcus Aurelius"
+	},
+	{
+		"text": "Don’t go on discussing what a good person should be. Just be one.",
+		"author": "Marcus Aurelius"
+	},
+	{
+		"text": "Remember that very little is needed to make a happy life.",
+		"author": "Marcus Aurelius"
+	},
+	{
+		"text": "Wealth consists not in having great possessions, but in having few wants.",
+		"author": "Epictetus"
+	},
+	{
+		"text": "Man is not worried by real problems so much as by his imagined anxieties about real problems.",
+		"author": "Epictetus"
+	},
+	{
+		"text": "First say to yourself what you would be; and then do what you have to do.",
+		"author": "Epictetus"
+	},
+	{
+		"text": "Any person capable of angering you becomes your master; he can anger you only when you permit yourself to be disturbed by him.",
+		"author": "Epictetus"
+	},
+	{
+		"text": "He who laughs at himself never runs out of things to laugh at.",
+		"author": "Epictetus"
+	},
+	{
+		"text": "Caretake this moment. Immerse yourself in its particulars. Respond to this person, this challenge, this deed. Quit evasions. Stop giving yourself needless trouble. It is time to really live; to fully inhabit the situation you happen to be in now.",
+		"author": "Epictetus"
+	},
+	{
+		"text": "A ship should not ride on a single anchor, nor life on a single hope.",
+		"author": "Epictetus"
+	},
+	{
+		"text": "It’s not what happens to you, but how you react to it that matters.",
+		"author": "Epictetus"
+	},
+	{
+		"text": "You become what you give your attention to.",
+		"author": "Epictetus"
+	},
+	{
+		"text": "No person is free who is not master of himself.",
+		"author": "Epictetus"
+	},
+	{
+		"text": "It’s not because things are difficult that we dare not venture. It’s because we dare not venture that they are difficult.",
+		"author": "Seneca"
+	},
+	{
+		"text": "Luck is what happens when preparation meets opportunity.",
+		"author": "Seneca"
+	},
+	{
+		"text": "We suffer more often in imagination than in reality.",
+		"author": "Seneca"
+	},
+	{
+		"text": "Associate with people who are likely to improve you.",
+		"author": "Seneca"
+	},
+	{
+		"text": "Hang on to your youthful enthusiasms – you’ll be able to use them better when you’re older.",
+		"author": "Seneca"
+	},
+	{
+		"text": "If a man knows not to which port he sails, no wind is favorable.",
+		"author": "Seneca"
+	},
+	{
+		"text": "He suffers more than necessary, who suffers before it is necessary.",
+		"author": "Seneca"
+	},
+	{
+		"text": "The whole future lies in uncertainty: live immediately.",
+		"author": "Seneca"
+	},
+	{
+		"text": "To wish to be well is a part of becoming well.",
+		"author": "Seneca"
+	},
+	{
+		"text": "He who is brave is free.",
+		"author": "Seneca"
 	}
 ]
